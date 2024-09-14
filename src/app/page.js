00611c240c54683df0bd9abddf123a7410eeef9c
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2024-09-14 09:19:38 -0700

    more stuff :)

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
+import { Button, ButtonGroup } from "@nextui-org/button";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className=" min-h-screen min-w-[100%]">
+      {/* <Button /> */}
       <main>
         <div className="bg-gray-300 h-screen flex flex-col">
           {/* Top section */}
-          <div className="flex justify-between items-center p-4">
+          <div className="flex justify-between p-4">
+            <Button className="bg-blue-200">quantization / subdivision</Button>
             <div className="bg-green-500 h-10 w-10 rounded-full"></div>
             <div className="text-center text-lg">
               <div className="bg-blue-900 w-64 h-4 mx-auto"></div>
@@ -19,9 +22,6 @@ export default function Home() {
           <div className="flex-1 flex">
             {/* Left sidebar */}
             <div className="w-1/5 bg-purple-900 flex flex-col p-4 justify-between">
-              <button className="bg-blue-900 text-white py-2">
-                quantization / subdivision
-              </button>
               <p className="text-white">AI assistant</p>
             </div>
 
